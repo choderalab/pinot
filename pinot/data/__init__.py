f7488741e565b961157b997b75903450631fdcf4
yuanqing-wang <wangyq@wangyq.net>
2020-04-28 15:33:40 -0400

    Update __init__.py

--- pinot/data/__init__.py
+++ pinot/data/__init__.py
@@ -1,8 +1,9 @@
+import pinot
 import pinot.data
-import pinot.data.utils as utils
+# import pinot.data.utils as utils
 import os
 
-esol = utils.from_csv(
+esol = pinot.data.utils.from_csv(
     os.path.dirname(
         utils.__file__) + '/esol.csv')
 
