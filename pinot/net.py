2bc7b8d967df4e77d18bc9607fe10f12f4ed11c6
yuanqing-wang <wangyq@wangyq.net>
2020-04-29 15:09:57 -0400

    net

--- pinot/net.py
+++ pinot/net.py
@@ -20,12 +20,14 @@ class Net(torch.nn.Module):
 
     def __init__(self, representation, parameterization,
             distribution_class=torch.distributions.normal.Normal,
-            param_transform=lambda x, y: (x, torch.abs(y))):
+            param_transform=lambda x, y: (x, torch.exp(y) + 1e-5),
+            expectation_fn=lambda x, y: x):
         super(Net, self).__init__()
         self.representation = representation
         self.parameterization = parameterization
         self.distribution_class = distribution_class
         self.param_transform = param_transform
+        self.expectation_fn = expectation_fn
 
     def forward(self, g):
         """ Forward pass.
@@ -50,23 +52,33 @@ class Net(torch.nn.Module):
 
         # parameterize the distribution according to the parameters
         # and the distribution class
+        
         distribution = self.distribution_class(
             *self.param_transform(
                 *torch.unbind(theta, dim=-1)))
 
         return distribution
 
-    def sample(self, g, n_samples):
+    def sample(self, g, n_samples=0):
         """ Parameterize a distribution and sample from it.
         """
         distribution = self.condition(g)
 
-        return distribution.sample(n_samples)
+        if n_samples == 0:
+            return distribution.sample()
+
+        return distribution.sample_n(n_samples)
+
+    def expectation(self, g):
+        theta = self.forward(g)
+        return self.expectation_fn(
+            *torch.unbind(theta, dim=-1))
 
     def loss(self, g, y):
         """ Compute the loss with a input graph and a set of parameters.
         """
+
         distribution = self.condition(g)
         
         return -distribution.log_prob(y)
-
+    
