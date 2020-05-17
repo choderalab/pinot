1a57cc42a7ab94485cb68c1c5c9f4ad9be0a4b66
yuanqing-wang <wangyq@wangyq.net>
2020-05-17 00:03:22 -0400

    rewrote net

--- pinot/net.py
+++ pinot/net.py
@@ -18,22 +18,78 @@ class Net(torch.nn.Module):
     learning, puts into a predicted distribution and calculates the
     corresponding divergence.
 
+
+    Attributes
+    ----------
+    representation: a `pinot.representation` module
+        the model that translates graphs to latent representations
+    output_regression: a `torch.nn.Moudle` or None,
+        if None, this will be set as a simple `Linear` layer that inputs
+        the latent dimension and output the number of parameters for
+        `self.distribution_class`
+    distribution_class: a `torch.distribution` object,
+        default=`torch.distributions.normal.Normal`
+        the class of distribution the model outputs
+    noise_model: either a string (
+        one of 
+            'homoschedastic',
+            'heteroschedastic',
+            'homoschedastic-fixed') 
+        or a function that transforms a set of parameters.
+
+
     """
 
     def __init__(
         self,
         representation,
-        parameterization,
+        output_regression=None,
         distribution_class=torch.distributions.normal.Normal,
-        param_transform=lambda x, y: (x, torch.exp(y) + 1e-5),
-        expectation_fn=lambda x, y: x,
+        noise_model='heteroschedastic',
     ):
+        
         super(Net, self).__init__()
         self.representation = representation
-        self.parametrization = parameterization
         self.distribution_class = distribution_class
-        self.param_transform = param_transform
-        self.expectation_fn = expectation_fn
+
+        # grab the last dimension of `representation`
+        regression_in_features = [
+                layer for layer in list(self.representation.modules())\
+                        if hasattr(layer, 'out_features')][-1].out_features
+
+        # TODO:
+        # make this less ugly
+        regression_out_features = len(distribution_class.arg_constraints)
+
+        if output_regression is None:
+            # make the output regression as simple as a linear one
+            # if nothing is specified
+            self._output_regression = torch.nn.ModuleList(
+                    [
+                        torch.nn.Linear(regression_in_features, 1)\
+                                for _ in range(regression_out_features)
+                    ])
+
+            def output_regression(theta):
+                return [f(theta) for f in self._output_regression]
+
+        self.output_regression = output_regression
+
+        # TODO:
+        # make this less ugly
+        if isinstance(noise_model, str):
+            if noise_model == 'heteroschedastic':
+                self.noise_model = lambda x : x
+
+            elif noise_model == 'homoschedasstic':
+                # NOTE: we assume noise parameter is the last one
+                self.LOG_SIGMA = torch.tensor(0.)
+                self.noise_model = lambda x: x[:-1] + \
+                        [self.LOG_SIGMA * torch.ones_like(x[-1])]
+
+            elif noise_model == 'homoschedastic':
+                self.noise_model = lambda x: x[:-1] + torch.ones_like(x[-1])
+            
 
     def forward(self, g):
         """ Forward pass.
@@ -46,8 +102,8 @@ class Net(torch.nn.Module):
         # latent representation $h$
         # ->
         # parameters $\theta$
-        theta = self.parametrization(h)
-
+        theta = self.output_regression(h)
+       
         return theta
 
     def condition(self, g):
@@ -56,28 +112,21 @@ class Net(torch.nn.Module):
         # get the parameters
         theta = self.forward(g)
 
+        # put theta through noise model
+        theta = self.noise_model(theta)
+
         # parameterize the distribution according to the parameters
         # and the distribution class
-
-        distribution = self.distribution_class(
-            *self.param_transform(*torch.unbind(theta, dim=-1))
-        )
+        distribution = self.distribution_class(*theta)
 
         return distribution
 
-    def sample(self, g, n_samples=0):
-        """ Parameterize a distribution and sample from it.
-        """
-        distribution = self.condition(g)
-
-        if n_samples == 0:
-            return distribution.sample()
-
-        return distribution.sample_n(n_samples)
-
     def expectation(self, g):
-        theta = self.forward(g)
-        return self.expectation_fn(*torch.unbind(theta, dim=-1))
+        # construct the distribution
+        distribution = self.condition(g)
+        
+        # expectation is the analytical mean of the distribution
+        return distribution.mean()
 
     def loss(self, g, y):
         """ Compute the loss with a input graph and a set of parameters.
--- pinot/tests/test_nets.py
+++ pinot/tests/test_nets.py
@@ -10,7 +10,22 @@ def test_initialize():
 
     layer = pinot.representation.dgl_legacy.GN
     net_representation = pinot.representation.Sequential(
-        layer, [32, "tanh", 32, "tanh", 32, "tanh"]
+        layer, [32, "tanh", 32, "tanh", 32, "tanh"], output_units=32,
     )
-    net_regression = pinot.regression.Linear(32, 1)
-    net = pinot.Net(net_representation, net_regression)
+    net = pinot.Net(net_representation)
+
+
+def test_forward():
+    import pinot
+    g, y = pinot.data.esol()[0]
+    layer = pinot.representation.dgl_legacy.GN
+    net_representation = pinot.representation.Sequential(
+        layer, [32, "tanh", 32, "tanh", 32, "tanh"], output_units=32,
+    )
+    net = pinot.Net(net_representation)
+
+    net.condition(g)
+
+
+test_forward()
+   
