35fe13fb391fb6e57b02a78c5877112783c68852
dnguyen1196 <dnguyen1196.github@gmail.com>
2020-05-18 14:27:25 -0400

    Quick fix via converting torch tensor to numpy and then back

--- scripts/generative/gvae_exp.py
+++ scripts/generative/gvae_exp.py
@@ -43,28 +43,31 @@ def gae_for(args):
     g = dgl.batch(gs)
 
     # get the adjacency matrix for the giant graph
-    adj = g.adjacency_matrix()
-
-    features = torch.cat((g.ndata["type"], g.ndata["h0"]), dim=1) # horizontal concat
-    print(features.shape)
+    adj = sp.coo_matrix(g.adjacency_matrix().to_dense().numpy())
+    features = torch.cat([g.ndata["type"], g.ndata["h0"]], dim=1)
     n_nodes, feat_dim = features.shape
 
+    # Store original adjacency matrix (without diagonal entries) for later
     adj_orig = adj
-    diag = adj_orig.to_dense().diagonal()
+    adj_orig = adj_orig - sp.dia_matrix((adj_orig.diagonal()[np.newaxis, :], [0]), shape=adj_orig.shape)
+    adj_orig.eliminate_zeros()
 
-    # Subtract the diagonal?
-    adj_orig = adj_orig - torch.diag(diag).to_sparse()
+    adj_train, train_edges, val_edges, val_edges_false, test_edges, test_edges_false = mask_test_edges(adj)
+    adj = adj_train
 
     # Some preprocessing
-    adj_label = torch.eye(adj.shape[0]) + adj
+    adj_norm = preprocess_graph(adj)
+    adj_label = adj_train + sp.eye(adj_train.shape[0])
     # adj_label = sparse_to_tuple(adj_label)
-    adj_label = torch.FloatTensor(adj_label)
+    adj_label = torch.FloatTensor(adj_label.toarray())
 
-    pos_weight = float(adj.shape[0] * adj.shape[0] -\
-         torch.sparse.sum(adj)) / torch.sparse.sum(adj)
+    pos_weight = float(adj_train.shape[0] * adj_train.shape[0] - adj_train.sum()) \
+         / adj_train.sum()
     
-    norm = adj.shape[0] * adj.shape[0] \
-        / float((adj.shape[0] * adj.shape[0] - torch.sparse.sum(adj)) * 2)
+    pos_weight = torch.FloatTensor([pos_weight])
+
+    norm = adj_train.shape[0] * adj_train.shape[0] \
+         / float((adj_train.shape[0] * adj_train.shape[0] - adj_train.sum()) * 2)
 
     model = GCNModelVAE(feat_dim, args.hidden1, args.hidden2, args.dropout)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
@@ -74,22 +77,27 @@ def gae_for(args):
         t = time.time()
         model.train()
         optimizer.zero_grad()
-        recovered, mu, logvar = model(features, adj_orig)
+        recovered, mu, logvar = model(features, adj_norm)
         loss = loss_function(preds=recovered, labels=adj_label,
                              mu=mu, logvar=logvar, n_nodes=n_nodes,
-                             norm=norm,
-                             pos_weight=torch.from_numpy(np.array([pos_weight])))
+                             norm=norm, pos_weight=pos_weight)
         loss.backward()
         cur_loss = loss.item()
         optimizer.step()
 
         hidden_emb = mu.data.numpy()
+        roc_curr, ap_curr = get_roc_score(hidden_emb, adj_orig, val_edges, val_edges_false)
 
         print("Epoch:", '%04d' % (epoch + 1), "train_loss=", "{:.5f}".format(cur_loss),
+              "val_ap=", "{:.5f}".format(ap_curr),
               "time=", "{:.5f}".format(time.time() - t)
               )
 
     print("Optimization Finished!")
+
+    roc_score, ap_score = get_roc_score(hidden_emb, adj_orig, test_edges, test_edges_false)
+    print('Test ROC score: ' + str(roc_score))
+    print('Test AP score: ' + str(ap_score))
 
 
 if __name__ == '__main__':
