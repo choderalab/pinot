4943a46adf25933c6b6c75ecd65ff5ca5c4c6e40
dnguyen1196 <dnguyen1196.github@gmail.com>
2020-05-18 18:58:11 -0400

    Renamed optimizer to loss for clarity

--- pinot/generative/torch_gvae/model.py
+++ pinot/generative/torch_gvae/model.py
@@ -6,18 +6,44 @@ from pinot.generative.torch_gvae.layers import GraphConvolution
 
 
 class GCNModelVAE(nn.Module):
+    """Graph convolutional neural networks for VAE
+    """
     def __init__(self, input_feat_dim, hidden_dim1, hidden_dim2, dropout):
+        """ Construct a VAE with GCN
+        """
         super(GCNModelVAE, self).__init__()
         self.gc1 = GraphConvolution(input_feat_dim, hidden_dim1, dropout, act=F.relu)
         self.gc2 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
         self.gc3 = GraphConvolution(hidden_dim1, hidden_dim2, dropout, act=lambda x: x)
-        self.dc = InnerProductDecoder(dropout, act=lambda x: x)
+        self.dc = InnerProductDecoder(dropout, act=lambda x: x) # Decoder
 
     def parameterization(self, x, adj):
+        """ Compute the parameters of the approximate Gaussian posterior
+         distribution
+        
+        Args:
+            x (FloatTensor): node features
+                Shape (N, D) where N is the number of nodes in the graph
+            adj (FloatTensor): adjacency matrix
+                Shape (N, N)
+
+        Returns:
+            (mu, logvar) (Float, Float): parameters of approximate posterior
+        """
         hidden1 = self.gc1(x, adj)
         return self.gc2(hidden1, adj), self.gc3(hidden1, adj)
 
     def inference(self, mu, logvar):
+        """ Returns a sample from the approximate posterior distribution
+
+        Args:
+            mu (Float): mean parameter
+            logvar (Float): log variance
+
+        Returns:
+            If during training, returns a sample from N(mu, exp(logvar))
+            If during testing, returns mu
+        """
         if self.training:
             std = torch.exp(logvar)
             eps = torch.randn_like(std)
@@ -26,6 +52,20 @@ class GCNModelVAE(nn.Module):
             return mu
 
     def forward(self, x, adj):
+        """ Forward pass through the GVAE
+
+        Args:
+            x (FloatTensor): node features
+                Shape (N, D) where N is the number of nodes in the graph
+            adj (FloatTensor): adjacency matrix
+                Shape (N, N)
+
+        Returns:
+            adj*, mu, logvar
+                adj* is the reconstructed adjacency matrix
+                mu, logvar are the parameters of the approximate Gaussian
+                    posterior
+        """
         mu, logvar = self.parameterization(x, adj)
         z = self.inference(mu, logvar)
         return self.dc(z), mu, logvar
