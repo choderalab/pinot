796871101ec27e826c0223ea1497cfb6b567b4f0
yuanqing-wang <wangyq@wangyq.net>
2020-05-19 13:23:26 -0400

    bbb

--- pinot/inference/bbb.py
+++ pinot/inference/bbb.py
@@ -9,7 +9,7 @@ import dgl
 # =============================================================================
 # MODULE CLASSES
 # =============================================================================
-class BBB(pinot.Sampler):
+class BBB(torch.optim.Optimizer):
     """ Gaussian Variational Posterior Bayesian-by-Backprop.
 
     """
@@ -19,19 +19,25 @@ class BBB(pinot.Sampler):
         super(BBB, self).__init__()
         self.optimizer = optimizer
 
-        # sigma here is initialized from a Gaussian distribution
-        # with dimensions matching the parameters
-        for group in self.optimizer.param_groups:
-            for p in group['params']:
-                if p.grad is None:
-                    continue
+        # TODO:
+        # make this compilable with more than one param group
+        assert len(self.optimizer.param_groups
+                ) == 1, 'Now we only support one param group.'
 
-                state = self.optimizer.state[p]
-                state['log_sigma'] = torch.distributions.normal.Normal(
-                    torch.zeros_like(p),
-                    initializer_std * torch.ones_like(p)
-                ).sample()
+        # copy the original param group
+        # this makes the hyperparameters stable
+        log_sigma_param_group = self.optimizer.param_groups[0].copy()
 
+        # initialize log_sigma
+        log_sigma_param_group['param'] = [torch.distributions.normal.Normal(
+            loc=torch.zeros_like(p),
+            scale=initializer_std * torch.ones_like(p))
+            for p in log_sigma_param_group['param']]
+
+        # append this to param_group
+        self.optimizer.add_param_group(
+                log_sigma_param_group)
+           
 
     @torch.no_grad()
     def step(self, closure):
@@ -46,40 +52,81 @@ class BBB(pinot.Sampler):
         # just in case
         loss = None
 
-        for group in self.optimizer.param_groups:
-            for p in group['params']:
-                if p.grad is None:
-                    continue
+        for p, log_sigma in zip(
+                *[
+                    self.optimizer.param_groups[0]['params'],
+                    self.optimizer.param_groups[1]['params']
+                ]):
 
-                state = self.optimizer.state[p]
-                
-                epsilon = torch.distributions.normal.Normal(
-                        torch.zeros_like(p),
-                        torch.ones_like(p)).sample()
-                
-                # clone mu and log_sigma
-                mu = p.clone()
-                log_sigma = state['log_sigma'].clone()
 
-                with torch.enable_grad():
-                    mu.requires_grad = True
-                    log_sigma.requires_grad=True
-                    
-                    # compute the kl loss term here
-                    kl_loss = torch.distributions.normal.Normal(
-                            loc=mu,
-                            scale=torch.exp(state['sigma'])).log_prob(p) -\
-                              self.theta_prior.log_prob(theta)
+            state = self.optimizer.state[p]
+            
+            # sample a noise
+            epsilon = torch.distributions.normal.Normal(
+                    torch.zeros_like(p),
+                    torch.ones_like(p)).sample()
+            
+            # clone mu and log_sigma
+            mu = p.detach().clone()
 
-                    
-                    d_kl_d_mu = torch.autograd(kl_loss, mu)
-                    d_kl_d_log_sigma = torch.autograd(kl_loss, log_sigma)
+            # perturb p to get theta
+            theta = p + epsilon * torch.exp(log_sigma.detach())
+            p.copy_(theta)
 
+            # calculate kl loss and the gradients thereof
+            with torch.enable_grad():
+                mu.requires_grad = True
+                theta.requires_grad = True
+                 
+                # compute the kl loss term here
+                kl_loss = torch.distributions.normal.Normal(
+                        loc=mu,
+                        scale=torch.exp(log_sigma)).log_prob(theta) -\
+                          self.theta_prior.log_prob(theta)
+            
+            d_kl_d_mu = torch.autograd.grad(kl_loss, mu)
+            d_kl_d_log_sigma = torch.autograd.grad(kl_loss, log_sigma)
+            d_kl_d_theta = torch.autograd.grad(kl_loss, theta)
+
+            # put the results in state dicts
+            state['d_kl_d_mu'] = d_kl_d_mu
+            state['d_kl_d_log_sigma'] = d_kl_d_log_sigma
+            state['d_kl_d_theta'] = d_kl_d_theta
+
+            # keep track of perturbation noise for cancellation later
+            state['mu'] = mu
+            state['epsilon'] = epsilon
+            
         # do one step with perturbed weights
         with torch.enable_grad():
             loss = closure()
 
+        for p, log_sigma in zip(
+                *[
+                    self.optimizer.param_groups[0]['params'],
+                    self.optimizer.param_groups[1]['params']
+                ]):
+                
+            state = self.optimizer.state[p]
+            
+            # cancel the perturbation
+            p.copy_(state['mu'])
 
+            # modify grad
+            p.grad.add_(state['d_kl_d_mu'] + state['d_kl_d_theta'])
+            log_sigma.grad.copy_(
+                    state['epsilon'] * (p.grad + state['d_kl_d_theta']) +\
+                            state['d_kl_d_log_sigma'])
+
+        
+        # update parameters based on whatever schedule
+        # `self.optimizer` proposes
+        self.optimizer.step()
+
+
+            
+
+                
 
         
         
