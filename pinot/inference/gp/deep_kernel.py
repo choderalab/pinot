3442f982d125b824be360868d0809a3b8136b7e7
yuanqing-wang <wangyq@wangyq.net>
2020-05-26 19:07:47 -0400

    deep kernel init

---
+++ pinot/inference/gp/deep_kernel.py
@@ -0,0 +1,31 @@
+#=============================================================================
+# IMPORTS
+# =============================================================================
+import torch
+import pinot
+import abc
+
+# =============================================================================
+# MODULE CLASSES
+# =============================================================================
+class DeepKernel(esp.gp.Kernel):
+    r""" A Gaussian Process Kernel with neural network representation.
+
+
+    """
+    def __init__(self, representation, base_kernel):
+        super(DeepKernel, self).__init__()
+        self.representation = representation
+        self.base_kernel = base_kernel
+
+    def forward(self, x, x_=None):
+        if x_ is None:
+            x_ = x
+
+        k = self.base_kernel.forward(
+                self.representation(x),
+                self.representation(x_))
+
+        return k
+        
+        
