b2de9d175154f94038f60a9c7fc6637485ca4827
Yuanqing Wang <wangyq@wangyq.net>
2020-05-26 18:55:28 -0400

    inference function for gp

--- pinot/inference/gp/kernel.py
+++ pinot/inference/gp/kernel.py
@@ -56,6 +56,42 @@ class Kernel(torch.nn.Module, abc.ABC):
     def inference(x_tr, y_tr, x_te, sigma=1.0):
         r""" Calculate the predictive distribution given `x_te`.
 
-
+        Parameters
+        ----------
+        x_tr : torch.tensor, shape=(batch_size, ...)
+            training data.
+        y_tr : torch.tensor, shape=(batch_size, 1)
+            training data measurement.
+        x_te : torch.tensor, shape=(batch_size, ...)
+            test data.
+        sigma : float or torch.tensor, shape=(), default=1.0
+            noise parameter.
         """
+        # compute the kernels
+        k_tr_tr = self.forward(x_tr, x_tr)
+        k_te_te = self.forward(x_te, x_te)
+        k_te_tr = self.forward(x_te, x_tr)
+        k_tr_te = self.forward(x_tr, x_te)
+
+        # (batch_size, batch_size)
+        k_plus_sigma = k_tr_tr + sigma ** 2 * torch.eye(k_tr_tr.shape[0])
+
+        # (batch_size, batch_size)
+        k_plus_sigma_inv = torch.inverse(k_plus_sigma)
+
+        # (batch_size*, batch_size)
+        k_te_tr_k_plus_sigma_inv = k_te_tr @ k_plus_sigma_inv
+
+        # (batch_size, measurement_dim)
+        expectation = k_te_tr_k_plus_sigma_inv @ y_tr
+
+        # (batch_size, batch_size)
+        covariance = k_te_te - k_te_tr_k_plus_sigma_inv @ k_tr_te
+
+        # construct noise predictive distribution
+        distribution = torch.distributions.multivariate_normal.MultivariateNormal(
+            expectation.flatten(),
+            covariance)
+
+        return distribution
 
