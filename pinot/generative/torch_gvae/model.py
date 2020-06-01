7523efc9316075b756dd6d567bb62a1f43eb185e
dnguyen1196 <dnguyen1196.github@gmail.com>
2020-06-01 18:50:25 -0400

    Added tests for combining net and vgae

--- pinot/generative/torch_gvae/model.py
+++ pinot/generative/torch_gvae/model.py
@@ -32,7 +32,9 @@ class GCNModelVAE(nn.Module):
         super(GCNModelVAE, self).__init__()
 
         # Decoder
-        self.dc = EdgeAndNodeDecoder(dropout, embedding_dim, num_atom_types)
+        # self.dc = EdgeAndNodeDecoder(embedding_dim, num_atom_types)
+        self.dc = SequentialDecoder(embedding_dim, num_atom_types)
+
         self.num_atom_types = num_atom_types
 
         # Encoder
@@ -201,7 +203,7 @@ class EdgeAndNodeDecoder(nn.Module):
     """ Decoder that returns both a predicted adjacency matrix
         and node identities
     """
-    def __init__(self, dropout, feature_dim, num_atom_types, hidden_dim=64):
+    def __init__(self, feature_dim, num_atom_types, hidden_dim=64, dropout=0):
         super(EdgeAndNodeDecoder, self).__init__()
         self.dropout = dropout
         self.hidden_dim = hidden_dim
@@ -227,3 +229,72 @@ class EdgeAndNodeDecoder(nn.Module):
         adj = torch.mm(z_prime, z_prime.t())
         node_preds = self.decode_nodes(z_prime)
         return (adj, node_preds)
+
+
+class SequentialDecoder(nn.Module):
+    """ Simple decoder where the node identity prediction is dependent
+    on the adjacency matrix.
+    Attributes
+    ----------
+    z_to_zx : torch.nn.Module
+        neural networks that map general latent space encoding $z$
+        to that for node prediction $z_x$
+    z_to_za : torch.nn.Module
+        neural networks that map general latent space encoding $z$
+        to that for edge prediction $z_a$
+    zx_to_x : torch.nn.Module
+        neural networks that map latent encoding of node $z_x$ to node
+        predictions $\hat{x}$
+    """
+
+    def __init__(self, embedding_dim, num_atom_types):
+        super(SequentialDecoder, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.num_atom_types = num_atom_types
+
+        self.Dx1 = 64
+        self.Dx2 = 64
+        self.z_to_zx = nn.Sequential(
+            nn.Linear(embedding_dim, self.Dx1),
+            nn.ReLU(),
+            nn.Linear(self.Dx1, self.Dx2)
+        )
+
+        self.Da1 = 64
+        self.Da2 = 64
+        self.z_to_za = nn.Sequential(
+            nn.Linear(embedding_dim, self.Da1),
+            nn.ReLU(),
+            nn.Linear(self.Da1, self.Da2)
+        )
+
+        self.hidden_dim = 64
+        self.zx_to_x = nn.Sequential(
+            nn.Linear(self.Dx2, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, self.num_atom_types)
+        )
+
+    def forward(self, z):
+        # (N, Dx)
+        zx = self.z_to_zx(z)
+        # (N, Da)
+        za = self.z_to_za(z)
+        # before rounding
+        # (N, N)
+        # A_tilde = F.sigmoid(za @ za.T)
+        A_tilde = za @ za.T
+        # round to yield predicted A
+        # (N, N)
+        A_hat = torch.where(
+            torch.gt(A_tilde, 0.5),
+            torch.ones_like(A_tilde),
+            torch.zeros_like(A_tilde))
+        # this is equivalent to one round of unnormalized message passing
+        # might need to add more
+        # (N, Dx)
+        zx = A_tilde @ zx
+        # predicted x
+        # (N, n_classes)
+        x_hat = self.zx_to_x(zx)
+        return (A_tilde, x_hat)
