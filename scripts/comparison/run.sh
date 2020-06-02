7323a7fe3f0787eacde35d1b6b3d4ba0919de339
yuanqing-wang <wangyq@wangyq.net>
2020-06-02 00:17:03 -0400

    data

--- scripts/comparison/run.sh
+++ scripts/comparison/run.sh
@@ -5,10 +5,10 @@ for opt in 'adam' 'bbb' 'sgld'
 do
     for layer in 'SAGEConv' 'GraphConv' 'EdgeConv'
     do
-        for lr in '1e-2' '1e-3' '1e-4'
+        for lr in '1e-3' '1e-4' '1e-6'
         do
             name="_"$opt"_"$layer"_"$lr
-            bsub -q gpuqueue -J $name -m "ld-gpu ls-gpu lt-gpu lg-gpu lu-gpu" -n 12 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=4] span[hosts=1]" -W 8:00 -o $name/%J.stdout -eo %J.stderr python ../../pinot/app/supervised_train.py --layer $layer --optimizer $opt --lr $lr --out $name --n_epochs 100
+            bsub -q gpuqueue -J $name -m "ld-gpu ls-gpu lt-gpu lg-gpu lu-gpu" -n 12 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=4] span[hosts=1]" -W 8:00 -o $name/%J.stdout -eo %J.stderr python ../../pinot/app/supervised_train.py --layer $layer --optimizer $opt --lr $lr --out $name --n_epochs 1000
 
 done
 done
