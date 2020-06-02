99d07f217488f51cbc8ac1c038e164bba86031a3
dnguyen1196 <dnguyen1196.github@gmail.com>
2020-06-01 20:51:45 -0400

    Modified cli to incorporate net + generative

--- pinot/app/cli.py
+++ pinot/app/cli.py
@@ -6,16 +6,25 @@ import pinot
 import os
 import numpy as np
 import torch
+from pinot.generative.torch_gvae import GCNModelVAE
 
 # =============================================================================
 # MODULE FUNCTIONS
 # =============================================================================
 def run(args):
-    layer = pinot.representation.dgl_legacy.gn(model_name=args.layer)
 
-    net_representation = pinot.representation.Sequential(
-        layer=layer,
-        config=args.config)
+    # get the entire dataset
+    ds= getattr(
+        pinot.data,
+        args.data)()
+
+    # Get the number of node features and initialize representation
+    # layer as a variational auto-encoder
+    input_feat_dim = ds[0][0].ndata["h"].shape[1]
+    net_representation = GCNModelVAE(input_feat_dim, 
+            gcn_type=args.layer,
+            gcn_hidden_dims=args.hidden_dims_gvae,
+            embedding_dim=args.embedding_dim)
 
     net = pinot.Net(
         net_representation,
@@ -25,10 +34,6 @@ def run(args):
         args.optimizer,
         lr=args.lr)(net)
 
-    # get the entire dataset
-    ds= getattr(
-        pinot.data,
-        args.data)()
 
     # not normalizing for now
     # y_mean, y_std, norm, unnorm = pinot.data.utils.normalize(ds)
@@ -75,20 +80,17 @@ def run(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--layer', default='GraphConv', type=str)
+    parser.add_argument('--layer', default='GraphConv', type=str, help="Type of graph convolution layer")
+    parser.add_argument('--hidden_dims_gvae',nargs='+', type=int, default=[128, 128], help="Hidden dimensions of the convolution layers")
+    parser.add_argument('--embedding_dim', type=int, default=64, help="embedding dimension (dimension of the encoder's output)")
     parser.add_argument('--noise_model', default='normal-heteroschedastic', type=str)
     parser.add_argument('--optimizer', default='adam', type=str)
-    parser.add_argument(
-        '--config',
-        nargs='*',
-        default=[32, 'tanh', 32, 'tanh', 32, 'tanh'])
-    parser.add_argument('--out', default='result', type=str)
-    parser.add_argument('--data', default='esol')
-    parser.add_argument('--batch_size', default=32, type=int)
-    parser.add_argument('--opt', default='Adam')
-    parser.add_argument('--lr', default=1e-5, type=float)
-    parser.add_argument('--partition', default='4:1', type=str)
-    parser.add_argument('--n_epochs', default=5)
+    parser.add_argument('--out', default='result', type=str, help="Folder to print out results to")
+    parser.add_argument('--data', default='esol', help="Data set name")
+    parser.add_argument('--batch_size', default=32, type=int, help="Batch size")
+    parser.add_argument('--lr', default=1e-5, type=float, help="Learning rate")
+    parser.add_argument('--partition', default='4:1', type=str, help="Training-testing split")
+    parser.add_argument('--n_epochs', default=5, help="Number of epochs")
 
     args = parser.parse_args()
     run(args)
