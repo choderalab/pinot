972874bc89c7f2e126c03eee95ba1ff059a3e148
yuanqing-wang <wangyq@wangyq.net>
2020-06-03 00:05:50 -0400

    data

--- scripts/gp_comparison/run.sh
+++ scripts/gp_comparison/run.sh
@@ -5,7 +5,7 @@ for opt in 'adam' 'bbb' 'sgld'
 do
     for layer in 'GraphConv' 'EdgeConv' 'SAGEConv' 'GINConv' 'SGConv' 'TAGConv' 
     do
-        for lr in '1e-5'
+        for lr in '1e-3' '1e-4' '1e-5' '1e-6'
         do
             name="_gp_"$opt"_"$layer"_"$lr
             bsub -q gpuqueue -J $name -m "ld-gpu ls-gpu lt-gpu lg-gpu lu-gpu" -n 12 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=4] span[hosts=1]" -W 8:00 -o $name/%J.stdout -eo %J.stderr python ../../pinot/app/gp_train.py --layer $layer --optimizer $opt --lr $lr --out $name --n_epochs 1000
