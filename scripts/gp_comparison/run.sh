166aa333760d16d2490a783c8c1857f6673a312b
yuanqing-wang <wangyq@wangyq.net>
2020-06-05 15:32:05 -0400

    data

--- scripts/gp_comparison/run.sh
+++ scripts/gp_comparison/run.sh
@@ -1,14 +1,14 @@
 #BSUB -q cpuqueue
 #BSUB -o %J.stdout
 
-for opt in 'adam' 'bbb' 'sgld'
+for opt in 'adam'
 do
     for layer in 'GraphConv' 'EdgeConv' 'SAGEConv' 'GINConv' 'SGConv' 'TAGConv' 
     do
-        for lr in '1e-3' '1e-4' '1e-5' '1e-6'
+        for lr in '1e-3' '1e-4' '1e-5'
         do
             name="_gp_"$opt"_"$layer"_"$lr
-            bsub -q gpuqueue -J $name -m "ld-gpu ls-gpu lt-gpu lg-gpu lu-gpu" -n 12 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=4] span[hosts=1]" -W 8:00 -o $name/%J.stdout -eo %J.stderr python ../../pinot/app/gp_train.py --layer $layer --optimizer $opt --lr $lr --out $name --n_epochs 1000
+            bsub -q gpuqueue -J $name -m "ld-gpu ls-gpu lt-gpu lg-gpu lu-gpu" -n 12 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=4] span[hosts=1]" -W 0:30 -o $name/%J.stdout -eo %J.stderr python ../../pinot/app/gp_train.py --layer $layer --optimizer $opt --lr $lr --out $name --n_epochs 1000
 
 done
 done
