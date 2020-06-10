adebdf73a4973a5de49b77677f5e4d870e2d3f76
yuanqing-wang <wangyq@wangyq.net>
2020-06-10 00:51:14 -0400

    data

--- pinot/data/__init__.py
+++ pinot/data/__init__.py
@@ -4,6 +4,9 @@ from . import utils
 import os
 
 esol = utils.from_csv(os.path.dirname(utils.__file__) + "/esol.csv")
+freesolv = utils.from_csv(os.path.dirname(utils.__file__) + "/SAMPL.csv", smiles_col=1, y_cols=[2])
+lipophilicity = utils.from_csv(os.path.dirname(utils.__file__) + "/Lipophilicity.csv")
+
 
 zinc_tiny = utils.load_unlabeled_data(os.path.dirname(utils.__file__) + "/zinc/all.txt", size=0.01)
 moses_tiny = utils.load_unlabeled_data(os.path.dirname(utils.__file__) + "/moses/all.txt", size=0.01)
