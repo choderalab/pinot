11938e395e8216080a485e0bfa254d5d5bd6aa5e
Yuanqing Wang <wangyq@wangyq.net>
2020-06-10 12:13:33 -0400

    fixed dimension and y_best

--- pinot/active/experiment.py
+++ pinot/active/experiment.py
@@ -123,7 +123,7 @@ class SingleTaskBayesianOptimizationExperiment(ActiveLearningExperiment):
 
         # get score
         # NOTE: y best here doesn't change argmax
-        score = self.acquisition(distribution)
+        score = self.acquisition(distribution, y_best=self.y_best)
 
         # argmax
         best = torch.argmax(score)
@@ -153,6 +153,10 @@ class SingleTaskBayesianOptimizationExperiment(ActiveLearningExperiment):
             n_epochs=self.n_epochs_training,
             net=self.net,
             record_interval=999999).train()
+
+        # grab y_max
+        gs, ys = old_data
+        self.y_best = torch.max(ys)
 
     def run(self, limit=999999):
         """ Run the model.
