c59174e06747cff7c233f3d16a353b09e6e8ad77
Yuanqing Wang <wangyq@wangyq.net>
2020-06-10 00:15:05 -0400

    Revert "Revert "Production""

--- pinot/inference/gp/gpr/__init__.py
+++ pinot/inference/gp/gpr/__init__.py
@@ -1,5 +1,6 @@
 import pinot.inference
 import pinot.inference.gp
 import pinot.inference.gp.gpr
+import pinot.inference.gp.gpr.base_gpr
 import pinot.inference.gp.gpr.exact_gpr
 import pinot.inference.gp.gpr.variational_gpr
