c720323bb4f4933e3f2a26e504d5783f980cc2d0
Michael Retchin <mrr2172@columbia.edu>
2020-06-12 14:09:12 -0400

    Removed needless parameter munging for GP initialization.

--- pinot/regression/models.py
+++ pinot/regression/models.py
@@ -2,7 +2,7 @@ import gpytorch
 import torch
 
 class ExactGPModel(gpytorch.models.ExactGP):
-    def __init__(self, batch_size, out_size, likelihood=gpytorch.likelihoods.GaussianLikelihood(),
+    def __init__(self, likelihood=gpytorch.likelihoods.GaussianLikelihood(),
                  mean_module=gpytorch.means.ConstantMean, prior=None,
                  kernel=gpytorch.kernels.RBFKernel):
         """
@@ -10,16 +10,11 @@ class ExactGPModel(gpytorch.models.ExactGP):
 
         Parameters
         ----------
-        batch_size : int
-        out_shape : int
         likelihood : gpytorch.likelihoods object
         mean_module : gpytorch.means object
         prior : float, 1d pytorch Tensor
         """
-        train_x = torch.rand((batch_size, out_size))
-        train_y = torch.rand((batch_size,))
-        
-        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        super(ExactGPModel, self).__init__(None, None, likelihood)
         self.num_outputs = 1
         self.covar_module = gpytorch.kernels.ScaleKernel(kernel())
         self.mean_module = mean_module()
