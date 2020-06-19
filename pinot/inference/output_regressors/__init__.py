8078bebf01b1ddd45c036281044a7d1f99300daf
Yuanqing Wang <wangyq@wangyq.net>
2020-06-19 17:49:22 -0400

    conflict

--- pinot/inference/output_regressors/__init__.py
+++ pinot/inference/output_regressors/__init__.py
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
+from pinot.inference.output_regressors import gaussian_process_output_regressor
+from pinot.inference.output_regressors import neural_network_output_regressor
+
 from pinot.inference.output_regressors.gaussian_process_output_regressor import (
 	ExactGaussianProcessOutputRegressor,
 	VariationalGaussianProcessOutputRegressor
 )
 
 from pinot.inference.output_regressors.neural_network_output_regressor import NeuralNetworkOutputRegressor
-=======
-from pinot.inference.output_regressors import gaussian_process_output_regressor
-from pinot.inference.output_regressors import neural_network_output_regressor
->>>>>>> f469527d16f5385a75c59ea65b54d8db06942da1
