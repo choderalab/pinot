18d358a73d67a189978ded21e3e963cf29d99f47
dnguyen1196 <dnguyen1196.github@gmail.com>
2020-06-18 21:00:49 -0400

    Added moonshot semi-supervised data

--- pinot/data/__init__.py
+++ pinot/data/__init__.py
@@ -3,6 +3,7 @@ import pinot.data
 from . import utils
 import os
 import numpy as np
+import sys
 
 esol = utils.from_csv(os.path.dirname(utils.__file__) + "/esol.csv")
 freesolv = utils.from_csv(os.path.dirname(utils.__file__) + "/SAMPL.csv", smiles_col=1, y_cols=[2])
@@ -42,3 +43,21 @@ moonshot_meta = utils.from_csv(
     smiles_col=0,
     y_cols=[3, 4, 5, 6, 7, 8, 9, 10, 11],
     scale=0.01)
+
+
+def load_moonshot_semi_supervised(unlabeled_size=0.1, seed=2666):
+    def load():
+        moonshot_labeled = moonshot() # Get labeled and unlabeled data
+        moonshot_unlabeled = utils.load_unlabeled_data(os.path.dirname(utils.__file__) + "/moonshot_activity_synthetic.txt", unlabeled_size, seed=seed)()
+        np.random.seed(seed)
+        moonshot_labeled.extend(moonshot_unlabeled)
+        np.random.shuffle(moonshot_labeled) # Combine and mix labeled and unlabeled data
+        return moonshot_labeled
+    return load
+
+# This data set contains around 6200 molecules, 530 are labeled
+moonshot_semi_small = load_moonshot_semi_supervised(0.1)
+
+# This data set contains around 60k molecules, 530 are labeled
+moonshot_semi_all   = load_moonshot_semi_supervised(1.0)
+
