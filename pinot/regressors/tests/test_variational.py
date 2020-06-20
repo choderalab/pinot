0eec5a82dd5c41bb688adf8c01f98f73bac90f80
Michael Retchin <mrr2172@columbia.edu>
2020-06-20 00:55:19 -0400

    Added a test.

---
+++ pinot/regressors/tests/test_variational.py
@@ -0,0 +1,24 @@
+import pytest
+import torch
+import pinot
+
+def test_variational():
+    import data
+    ds = data.esol()
+    ds_batched = data.utils.batch(ds, 32)
+
+    representation = pinot.representation.Sequential(pinot.representation.dgl_legacy.gn(),
+                                                 [32, 'tanh', 32, 'tanh', 32, 'tanh'])
+
+    from pinot.regressors import VariationalGaussianProcessRegressor
+    net = pinot.Net(
+        representation,
+        VariationalGaussianProcessRegressor
+    ) # .to(torch.device('cuda:0'))
+    
+    from pinot.app.experiment import Train
+    net = Train(net=net,
+            data=ds_batched,
+            n_epochs=10,
+            optimizer=torch.optim.Adam(net.parameters(), lr=1e-4),
+            marginal_likelihood=gpytorch.mlls.VariationalELBO).train()
