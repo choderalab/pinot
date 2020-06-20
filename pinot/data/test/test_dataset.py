5a909c9b3ec7c70bf0feabc1f3933fb31ec0d9a8
yuanqing-wang <wangyq@wangyq.net>
2020-06-20 12:12:41 -0400

    test

---
+++ pinot/data/test/test_dataset.py
@@ -0,0 +1,15 @@
+import pytest
+
+def test_import():
+    import pinot.data.datasets
+
+
+@pytest.fixture
+def esol():
+    import pinot
+    ds = pinot.data.datasets.Dataset(
+            ).from_csv('esol.csv')
+
+
+def test_esol(esol):
+    esol
