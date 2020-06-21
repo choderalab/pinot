503a85a013d16480f6b294ef1b92172266054d28
dnguyen1196 <dnguyen1196.github@gmail.com>
2020-06-21 16:36:59 -0400

    working gpu script

--- scripts/semi_supervised/run_semi_learn_curve.sh
+++ scripts/semi_supervised/run_semi_learn_curve.sh
@@ -1,13 +1,11 @@
-#BSUB -q cpuqueue
-#BSUB -o %J.stdout
 
+logfolder="$PWD/semi-supervised-logs"
 
-for layer in 'GraphConv'
+for regressor in 'gp' 'vgp' 'nn'
 do
     for architecture in '128 tanh 128 tanh' '64 tanh 64 tanh' '64 tanh 64 tanh 64 tanh'
     do
-        name="_"$opt"_"$layer"_"$lr
-        bsub -q gpuqueue -J $name -m "ld-gpu ls-gpu lt-gpu lg-gpu lu-gpu" -n 12 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=4] span[hosts=1]" -W 0:30 -o $name/%J.stdout -eo %J.stderr python pinot/scripts/semi_supervised/semi_supervised_comparison.py --layer $layer --optimizer $opt --architecture $architecture --n_epochs 500 --cuda
-
+        name="${regressor}_${architecture}"
+        bsub -q gpuqueue -J gpu-semi -m "ld-gpu ls-gpu lt-gpu lg-gpu lu-gpu" -n 12 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=16] span[hosts=1]" -W 10:00 -o "${logfolder}/${name}.stdout" -e "${logfolder}/${name}.stderr" /home/wangy1/miniconda3/envs/pinot/bin/python scripts/semi_supervised/semi_supervised_comparison.py --n_epochs 100 --cuda --regressor_type $regressor --architecture $architecture --log "${name}.logs" 
     done
 done
