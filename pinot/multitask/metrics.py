582ea377f7da6cb7d56f25674a79046ba8ddc4cc
Michael Retchin <mrr2172@columbia.edu>
2020-06-22 17:50:17 -0400

    Fixed tab/space problem with metrics file.

--- pinot/multitask/metrics.py
+++ pinot/multitask/metrics.py
@@ -27,14 +27,14 @@ def _mse(y, y_hat):
 
 
 def mse(net, g, y, l, sampler=None):
-	task_metrics = {}
-	for task, mask in enumerate(l.T):
-	    y_hat_task = net.condition(g, sampler=sampler).mean.cpu()[mask]
-	    y_task = y.cpu()[mask]
+    task_metrics = {}
+    for task, mask in enumerate(l.T):
+        y_hat_task = net.condition(g, sampler=sampler).mean.cpu()[mask]
+        y_task = y.cpu()[mask]
 
-	    # gp
-	    if y_hat_task.dim() == 1:
-	        y_hat_task = y_hat_task.unsqueeze(1)
+        # gp
+        if y_hat_task.dim() == 1:
+            y_hat_task = y_hat_task.unsqueeze(1)
         task_metrics[task] = _mse(y_task, y_hat_task)
     return task_metrics
 
@@ -47,14 +47,15 @@ def _rmse(y, y_hat):
 
 
 def rmse(net, g, y, l, sampler=None):
-	task_metrics = {}
-	for task, mask in enumerate(l.T):
-	    y_hat_task = net.condition(g, sampler=sampler).mean.cpu()[mask]
-	    y_task = y.cpu()[mask]
+    task_metrics = {}
+    for task, mask in enumerate(l.T):
+        y_hat_task = net.condition(g, sampler=sampler).mean.cpu()[mask]
+        y_task = y.cpu()[mask]
 
-	    # gp
-	    if y_hat_task.dim() == 1:
-	        y_hat_task = y_hat_task.unsqueeze(1)
+        # gp
+        if y_hat_task.dim() == 1:
+            y_hat_task = y_hat_task.unsqueeze(1)
+            
         task_metrics[task] = _rmse(y_task, y_hat_task)
     return task_metrics
 
@@ -86,14 +87,14 @@ def pearsonr(net, g, y, l, sampler=None):
     task_metrics = {}
 
     for task, mask in enumerate(l.T):
-	    y_hat_task = net.condition(g, task, sampler=sampler).mean.detach().cpu()[mask]
-	    y_task = y.detach().cpu()[mask]
-	    
-	    result = pr(y_task.flatten().numpy(),
-	    			y_hat_task.flatten().numpy())
+        y_hat_task = net.condition(g, task, sampler=sampler).mean.detach().cpu()[mask]
+        y_task = y.detach().cpu()[mask]
+        
+        result = pr(y_task.flatten().numpy(),
+                    y_hat_task.flatten().numpy())
 
-	    correlation, _ = result
-	    task_metrics[task] = torch.Tensor([correlation])[0]
+        correlation, _ = result
+        task_metrics[task] = torch.Tensor([correlation])[0]
     return task_metrics
 
 
@@ -110,20 +111,20 @@ def avg_nll(net, g, y, l, sampler=None):
     task_metrics = {}
 
     for task, mask in enumerate(l.T):
-	    # # ensure `y` is one-dimensional
-	    # assert ((y.dim() == 2 and y.shape[-1] == 1) or 
-	    #     (y.dim() == 1)
-	    # )
+        # # ensure `y` is one-dimensional
+        # assert ((y.dim() == 2 and y.shape[-1] == 1) or 
+        #     (y.dim() == 1)
+        # )
 
-	    # create dummy ys if unlabeled
-		y_dummy = torch.zeros(y.shape[0])
+        # create dummy ys if unlabeled
+        y_dummy = torch.zeros(y.shape[0])
 
-		if torch.cuda.is_available():
-			y_dummy = y_dummy.to(torch.device('cuda:0'))
+        if torch.cuda.is_available():
+            y_dummy = y_dummy.to(torch.device('cuda:0'))
 
-		y_dummy[mask] = y[mask, task]
+        y_dummy[mask] = y[mask, task]
 
-	    # calculate the log_prob
-	    log_prob = distributions[task].log_prob(y_dummy.flatten()).mean()
-	    task_metrics[task] = -log_prob
+        # calculate the log_prob
+        log_prob = distributions[task].log_prob(y_dummy.flatten()).mean()
+        task_metrics[task] = -log_prob
     return task_metrics
