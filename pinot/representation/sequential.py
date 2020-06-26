87d73ce6143a569bb0535b50eab37ba90fd77dcd
dnguyen1196 <dnguyen1196.github@gmail.com>
2020-06-26 18:41:27 -0400

    sequential attention network

--- pinot/representation/sequential.py
+++ pinot/representation/sequential.py
@@ -3,7 +3,7 @@
 import pinot
 import torch
 import dgl
-
+import numpy as np
 
 class Sequential(torch.nn.Module):
     def __init__(
@@ -79,6 +79,95 @@ class Sequential(torch.nn.Module):
                 x = pool(g)
 
         return x
+
+    def pool(self, g, h, pool=lambda g: dgl.sum_nodes(g, "h")):
+        with g.local_scope():
+            g.ndata["h"] = h
+            return pool(g)
+
+
+class SequentialAttention(torch.nn.Module):
+    """ Sequential module for Attention Graph Networks
+    It follows very similar interface to Sequential. 
+
+    Config should be a sequence of 
+    
+    [out_features, num_heads, pool_action, activation, out_features, ...]
+
+    """
+    def __init__(
+        self,
+        config,
+        feature_units=117,
+        input_units=128,
+        model_kwargs={},
+    ):
+        super(SequentialAttention, self).__init__()
+
+        # the initial dimensionality
+        dim = input_units
+        assert(len(config) % 4 == 0)
+        layers = np.split(np.array(config, dtype=object), int(len(config)/4))
+
+        # initial featurization
+        self.f_in = torch.nn.Sequential(
+            torch.nn.Linear(feature_units, input_units), torch.nn.Tanh()
+        )
+
+        # record the name of the layers in a list
+        self.exes = []
+        self.pool_layers = []
+        prev_concat = False
+        for idx, layer in enumerate(layers):
+            out_features, num_heads, pool, activation = layer
+
+            assert(pool in ["sum", "average", "concat"])
+
+            # Initialize GATLayer
+            gatLayer = dgl.nn.pytorch.GATConv(dim, out_features, num_heads, **model_kwargs)
+            setattr(self, "d" + str(idx), gatLayer)
+            dim = out_features if pool != "concat" else out_features * num_heads
+            self.exes.append("d" + str(idx))
+            
+            # Activation layer
+            activation = getattr(torch, activation)
+            setattr(self, "a" + str(idx), activation)
+            self.exes.append("a" + str(idx))
+
+            # Pooling
+            self.pool_layers.append(pool)
+
+        # This module is initialized here and not used
+        # 'Net' automatically determines the input dimension to output regressor by inspecting
+        # the last out_features of 'Representation'. In the case of AttentionNetworks, the output
+        # is 3D (due to multiple heads)
+        self.dummy_output = torch.nn.Linear(1, out_features)
+
+    def forward(self, g, h=None, pool=lambda g: dgl.sum_nodes(g, "h")):
+        if h is None:
+            h = g.ndata["h"]
+        h = self.f_in(h)
+        pool_id = 0
+
+        with g.local_scope():
+            for exe in self.exes:
+                if exe.startswith("d"):
+                    h = getattr(self, exe)(g, h)
+                    if self.pool_layers[pool_id] == "concat":
+                        h = h.view(h.shape[0], -1)
+                    elif self.pool_layers[pool_id] == "average":
+                        h = h.mean(1)
+                    else:
+                        h = h.sum(1)
+                    pool_id += 1
+                else:
+                    h = getattr(self, exe)(h)
+
+            if pool is not None:
+                g.ndata["h"] = h
+                h = pool(g)
+
+            return h
 
     def pool(self, g, h, pool=lambda g: dgl.sum_nodes(g, "h")):
         with g.local_scope():
