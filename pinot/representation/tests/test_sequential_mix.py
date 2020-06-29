1e1c3d0f82e3ec8ea4f3c98d3231c855a59b6606
dnguyen1196 <dnguyen1196.github@gmail.com>
2020-06-29 13:34:02 -0400

    test sequential mix

---
+++ pinot/representation/tests/test_sequential_mix.py
@@ -0,0 +1,8 @@
+import pinot
+
+
+def test_setup():
+    """ """
+    config = ["GraphConv", 128, "activation", "tanh", "dropout", 0.1,\
+            "GATConv", 128, 3, "attention_pool", "mean", "activation", "tanh"]
+    nets = pinot.representation.SequentialMix(config)
