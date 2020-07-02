093b39899303adf2daa6173852efe56dbd00d1a8
Theofanis Karaletsos <theofanis@uber.com>
2020-07-01 21:02:33 -0700

    updating bio[hsyical regressor class for roudn 1 of comment

--- pinot/regressors/biophysical_regressor.py
+++ pinot/regressors/biophysical_regressor.py
@@ -5,11 +5,6 @@ import torch
 import pinot
 import abc
 import math
-from pinot.regressors.base_regressor import BaseRegressor
-from pinot.regressors.gaussian_process_regressor import ExactGaussianProcessRegressor
-from pinot.regressors.neural_network_regressor import NeuralNetworkRegressor
-
-# import gpytorch
 import numpy as np
 
 class BiophysicalRegressor(torch.nn.Module):
@@ -29,32 +24,28 @@ class BiophysicalRegressor(torch.nn.Module):
     def __init__(self, base_regressor=None):
         super(BiophysicalRegressor, self).__init__()
         self.base_regressor = base_regressor
-        self.log_sigma_measurement = torch.zeros(1)
+        self.log_sigma_measurement = torch.nn.Parameter(torch.zeros(1))
 
 
-	def g(self, func_value=None, test_ligand_concentration=1e3-):
+	def g(self, func_value=None, test_ligand_concentration=1e-3):
 	    return 1 / (1 + torch.exp(-func_value) / test_ligand_concentration)
 
 
 	def condition(self, h=None, test_ligand_concentration=1e-3, **kwargs):
-        distribution_base_regressor = self.base_regressor.condition(h, kwargs)
-
-        #we sample from the latent f to paush things through the likelihood
-        #Note: if we do this, in order to get good estimates of LLK we may need to draw multiple samples
-        f_sample = distribution_base_regressor.rsample()
-
-        mu_m = self.g(func_value=f_sample, test_ligand_concentration=test_ligand_concentration)
-        sigma_m = torch.exp(self.log_sigma_measurement)
-
-        distribution_measurement = torch.distributions.normal.Normal(loc=mu_m, scale=sigma_m)
-
-        return distribution_measurement
+		distribution_base_regressor = self.base_regressor.condition(h, **kwargs)
+		#we sample from the latent f to push things through the likelihood
+		#Note: if we do this, in order to get good estimates of LLK we may need to draw multiple samples
+		f_sample = distribution_base_regressor.rsample()
+		mu_m = self.g(func_value=f_sample, test_ligand_concentration=test_ligand_concentration)
+		sigma_m = torch.exp(self.log_sigma_measurement)
+		distribution_measurement = torch.distributions.normal.Normal(loc=mu_m, scale=sigma_m)
+		return distribution_measurement
 
 
-    def loss(self, h=None, y=None, test_ligand_concentration=1e-3, **kwargs):
-    	distribution_measurement = self.condition(h=h, test_ligand_concentration=test_ligand_concentration, kwargs)
-    	loss_measurement = -distribution_measurement.log_prob(y)
-    	return loss_measurement
+	def loss(self, h=None, y=None, test_ligand_concentration=1e-3, **kwargs):
+		distribution_measurement = self.condition(h=h, test_ligand_concentration=test_ligand_concentration, **kwargs)
+		loss_measurement = -distribution_measurement.log_prob(y)
+		return loss_measurement
 
 
    	def marginal_sample(self, h=None, n_samples=100, test_ligand_concentration=1e-3, **kwargs):
@@ -73,7 +64,7 @@ class BiophysicalRegressor(torch.nn.Module):
     	"""
     	sample n_samples often from loss in order to get a better approximation
     	"""
-    	distribution_base_regressor = self.base_regressor.condition(h, kwargs)
+    	distribution_base_regressor = self.base_regressor.condition(h, **kwargs)
     	marginal_loss_measurement = 0
     	for ns in range(n_samples):
     		for ns in range(n_samples):
