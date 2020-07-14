99f3aedd6d68c3a9b7a02ca42a4ea116158c8bc9
Michael Retchin <mrr2172@columbia.edu>
2020-07-13 22:28:38 -0400

    Added loop to the bash script.

--- scripts/active/acquisition_loop.sh
+++ scripts/active/acquisition_loop.sh
@@ -1,7 +1,10 @@
 for index in 0 1 2 3 4 5 6 7 8 9 10
 do
-	for acquisition in 'ThompsonSampling' 'WeightedSamplingExpectedImprovement' 'WeightedSamplingProbabilityOfImprovement' 'WeightedSamplingUpperConfidenceBound' 'GreedyExpectedImprovement' 'GreedyProbabilityOfImprovement' 'GreedyUpperConfidenceBound' 'BatchRandom' 'BatchTemporal'
+	for q in 5 10 96
 	do
-		python plotting_active.py --acquisition $acquisition --q 10 --index $index
+		for acquisition in 'ThompsonSampling' 'WeightedSamplingExpectedImprovement' 'WeightedSamplingProbabilityOfImprovement' 'WeightedSamplingUpperConfidenceBound' 'GreedyExpectedImprovement' 'GreedyProbabilityOfImprovement' 'GreedyUpperConfidenceBound' 'BatchRandom' 'BatchTemporal'
+		do
+			python plotting_active.py --acquisition $acquisition --q 10 --index $index
+		done
 	done
 done
