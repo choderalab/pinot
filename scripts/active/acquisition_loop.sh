35cb51e6979c1ba7f48c0daea1a9d06cc91016ce
Michael Retchin <mrr2172@columbia.edu>
2020-07-13 22:29:01 -0400

    Fixed typo.

--- scripts/active/acquisition_loop.sh
+++ scripts/active/acquisition_loop.sh
@@ -4,7 +4,7 @@ do
 	do
 		for acquisition in 'ThompsonSampling' 'WeightedSamplingExpectedImprovement' 'WeightedSamplingProbabilityOfImprovement' 'WeightedSamplingUpperConfidenceBound' 'GreedyExpectedImprovement' 'GreedyProbabilityOfImprovement' 'GreedyUpperConfidenceBound' 'BatchRandom' 'BatchTemporal'
 		do
-			python plotting_active.py --acquisition $acquisition --q 10 --index $index
+			python plotting_active.py --acquisition $acquisition --q $q --index $index
 		done
 	done
 done
