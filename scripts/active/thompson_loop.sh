57a8c6cd1de2e61550c5a4e6b939dca009b16c69
Michael Retchin <mrr2172@columbia.edu>
2020-07-15 23:45:47 -0400

    Fewer epochs.

--- scripts/active/thompson_loop.sh
+++ scripts/active/thompson_loop.sh
@@ -4,7 +4,7 @@ do
 	do
 		for acquisition in 'ThompsonSampling' 'ExpectedImprovement' 'ProbabilityOfImprovement' 'UpperConfidenceBound' 'Random' 'Human'
 		do
-			for num_epochs in 400 600
+			for num_epochs in 600
 			do
 			 bsub -q gpuqueue -m "ld-gpu ls-gpu lt-gpu lg-gpu lu-gpu" -n 4 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=4] span[hosts=1]" -W 1:00 -o %J.stdout -eo %J.stderr python thompson_sampling_plot.py --num_epochs $num_epochs --acquisition $acquisition --q $q --index $index --data moonshot
 			done
