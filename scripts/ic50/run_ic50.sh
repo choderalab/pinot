64ae7a3af65a9332276ca6adf4fc35fd039079a8
Michael Retchin <miretchin@gmail.com>
2021-02-26 10:19:49 -0500

    Added ic50 script.

---
+++ scripts/ic50/run_ic50.sh
@@ -0,0 +1,32 @@
+#BSUB -o %J.stdout
+for units in 32
+do
+    for unit_type in "GraphSAGE"
+    do
+        for n_layers in 3 4
+        do
+            layer="$unit_type $units activation tanh "
+            architecture=$(for a in `eval echo {1..$n_layers}`; do echo $layer; done)
+            for inducing_pt in 80
+            do
+                for annealing in 1.0
+                do
+                    for regressor in 'vgp'
+                    do
+			for normalize in 0
+			do
+			    for seed in 0
+			    do
+                            		name="${regressor}_${n_layers}_${unit_type}_${units}_${sample_frac}_${annealing}_${inducing_pt}_${normalize}_${seed}"
+                            		bsub -q gpuqueue -n 2 -gpu "num=1:j_exclusive=yes" -R "rusage[mem=8] span[hosts=1]" -W 15:00 -o "logs_$name.stdout" -eo "logs_$name.stderr" \
+			    		python3 ic50_supervised.py --data moonshot_pic50 --n_epochs 350 --cuda --regressor_type $regressor --architecture $architecture \
+				        --log "${name}.logs" --record_interval 50 --n_inducing_points $inducing_pt --annealing $annealing \
+			    		--normalize $normalize --time_limit "14:00" --filter_outliers --fix_seed --seed $seed --output "/data/chodera/retchinm/pIC50"
+			    done
+	    	        done
+                    done
+                done
+            done
+        done
+    done
+done
