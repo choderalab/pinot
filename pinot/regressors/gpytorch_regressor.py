e735b8af1a7d1129b938e0f231e5e332e5619fa1
dnguyen1196 <dnguyen1196.github@gmail.com>
2021-03-25 20:39:30 -0400

    updated vgp based on gpytorch

--- pinot/regressors/gpytorch_regressor.py
+++ pinot/regressors/gpytorch_regressor.py
@@ -1,5 +1,4 @@
 import torch
-import math
 import gpytorch
 from gpytorch.variational import CholeskyVariationalDistribution
 from gpytorch.variational import VariationalStrategy
@@ -7,11 +6,10 @@ from gpytorch.models import ApproximateGP
 
 
 class VariationalGP(ApproximateGP):
-    def __init__(self, in_features, num_data, inducing_points=None, n_inducing_points=100,
-                 mean=None, covar=None, beta=1.0):
+    def __init__(self, in_features, inducing_points=None, n_inducing_points=100,
+                 mean=None, covar=None, beta=1e-2, variational_dist="cholesky"):
         """
         :param in_features: dimension of the input to GP layer
-        :param num_data: total number of training samples
         :param inducing_points: inducing points, second dimension should be the same as in_features
         :param num_inducing_points: number of inducing points, if inducing_points are not given, will be used
             to randomly initialize inducing points
@@ -23,26 +21,37 @@ class VariationalGP(ApproximateGP):
             # Randomly initialize inducing points
             inducing_points = torch.rand(n_inducing_points, in_features)
 
-        variational_distribution = CholeskyVariationalDistribution(
-            inducing_points.size(-2)
-        )
+        if variational_dist == "cholesky":
+            variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(inducing_points.size(-2))
+        elif variational_dist == "delta":
+            variational_distribution = gpytorch.variational.DeltaVariationalDistribution(inducing_points.size(-2))
+        elif variational_dist == "meanfield":
+            variational_distribution = gpytorch.variational.MeanFieldVariationalDistribution(inducing_points.size(-2))
+        else:
+            variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(inducing_points.size(-2))
 
         # LMCVariationalStrategy for introducing correlation among tasks if we want MultiTaskGP
         variational_strategy = VariationalStrategy(
             self, inducing_points, variational_distribution, learn_inducing_locations=True
         )
         super(VariationalGP, self).__init__(variational_strategy)
+
         self.mean_module = gpytorch.means.LinearMean(in_features) if mean is None else mean
-        self.covar_module = gpytorch.kernels.ScaleKernel(
-            gpytorch.kernels.RBFKernel(
-                lengthscale_prior=gpytorch.priors.SmoothedBoxPrior(
-                    math.exp(-1), math.exp(1), sigma=0.1, transform=torch.exp
-                )
-            )
-        ) if covar is None else covar
+
+        if covar is None:
+            self.covar_module = gpytorch.kernels.RBFKernel()
+        elif isinstance(covar, str):
+            if covar == "RBFKernel":
+                self.covar_module = gpytorch.kernels.RBFKernel()
+            elif covar == "LinearKernel":
+                self.covar_module = gpytorch.kernels.LinearKernel()
+            else:
+                self.covar_module = gpytorch.kernels.PolynomialKernel(3)
+        else:
+            self.covar_module = covar
+
         self.num_inducing = inducing_points.size(-2)
         self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
-        self.num_data = num_data
         self.beta = beta
 
     def forward(self, h, **kwargs):
@@ -67,7 +76,8 @@ class VariationalGP(ApproximateGP):
         # Compute the log-likelihood and the KL divergence, following the same steps as in function
         # forward() of _ApproximateMarginalLogLikelihood
         log_likelihood = self.likelihood.expected_log_prob(y, approximate_dist_f, **kwargs).sum(-1).div(num_batch)
-        kl_divergence = self.variational_strategy.kl_divergence().div(self.num_data).mul(self.beta)
+        # kl_divergence = self.variational_strategy.kl_divergence().div(self.num_data).mul(self.beta)
+        kl_divergence = self.variational_strategy.kl_divergence().mul(self.beta)
         elbo = log_likelihood - kl_divergence
         return -(elbo.sum())
 
