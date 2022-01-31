1627b550a4e8a47cfad43801924fae46621202c2
miretchin <miretchin@users.noreply.github.com>
2022-01-30 22:42:51 -0500

    Changing filename.

--- scripts/hts/hts_supervised_functional.py
+++ scripts/hts/hts_supervised_functional.py
@@ -35,7 +35,8 @@ def run(args):
                 f'_wd={args.weight_decay}_lsp={args.label_split[0]}_frac={args.sample_frac}'
                 f'_anneal={args.annealing}_induce={args.n_inducing_points}_normalize={args.normalize}'
                 f'_{args.index}_seed={seed}_filterthreshold={args.filter_threshold}'
-                f'_mumean={args.mu_mean}_mustd={args.mu_std}_stdvalue={args.std_value}')
+                f'_mumean={args.mu_mean}_mustd={args.mu_std}_stdvalue={args.std_value}'
+                f'_filter_neg_train={args.filter_neg_train}_filter_neg_test={args.filter_neg_test}')
 
     print(savefile)
     logging.debug("savefile = {}".format(savefile))
